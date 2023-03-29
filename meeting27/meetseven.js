d1bc80050e865b01619299829724fc1731121b72
lekso <leqsokacho@gmail.com>
2023-03-30 03:09:09 +0400

    last exercise

---
+++ meeting27/meetseven.js
@@ -0,0 +1,122 @@
+        // ამით გადასვიჩე დავალებები ზუკიჩ <3 
+
+          /* <3 პირველი დავალება  */
+
+// let userIsLogedIn = true; // მომხმარებელი შესულია.
+// let videoIsCommentable = true; // კომენტარის წვდომა ჩართულია.
+
+// const userCanComment = userIsLogedIn && videoIsCommentable;
+
+// console.log(userCanComment); 
+
+
+
+
+          /* <3 მეორე დავალება */
+
+// let userIsLoggedIn = true; // მომხმარებელი შესულია.
+// let userIsBlocked = false; // მომხარებელი დაბლოკილი არ არის.
+
+// const userCanPost = userIsLoggedIn || userIsBlocked;
+
+// console.log(userCanPost);
+
+
+
+
+          /* <3 მესამე დავალება */
+
+// let balance = 50; // მომხმარებლის ბალანსი.
+// let userCartTotal = 25; // შენაძენის ღირებულება.
+
+// const paymentComplate = userCartTotal <= balance;
+
+// console.log(paymentComplate);
+
+
+
+
+          /* <3 მეოთხე დავალება */
+
+// let userIsAdmin = true; // მომხმარებელი ადმინია.
+// let userIsModerator = true; // მომხმარებელი მოდერატორია.
+// let userIsCommentAuthor = true; // მომხმარებელი კომენტარის ავტორია.
+
+// const userCanDelete = userIsAdmin || userIsModerator || userIsCommentAuthor;
+
+// console.log(userCanDelete);
+
+
+
+
+          /* <3 მეხუთე დავალება */
+    
+// let presidentKey = true;
+// let primeMinisterKey = true;
+// let generalOfaArmiesKey = true;
+// let masterKey = true;
+
+// const bombWillLaunch = presidentKey && primeMinisterKey && generalOfaArmiesKey || masterKey;
+
+// console.log(bombWillLaunch);
+
+
+
+
+          /* <3 მეექვსე დავალება  */
+
+// const userPinConde = 123;
+// const currentPinCode = 123;
+
+// const newPinCode = 333;
+// const retypeNewPinCode = 333;
+
+// const pinChangeComplate = currentPinCode && newPinCode && retypeNewPinCode;
+
+// console.log(!!pinChangeComplate); 
+
+
+
+
+          /* <3 მეშვიდე დავალება */
+
+// let health = 80;
+// let lives = 1;
+
+// const gameOver = (80 < 1) && (1 < 1);
+
+// console.log(!gameOver);
+
+
+
+
+          /* <3 მერვე დავალება */
+
+// let capsuleCount = 5;
+// let capsulePrice = 1.5;
+// let userBalance = 3;
+// let creditCardStatus = true;
+
+// const paymentComplate = (capsuleCount - capsulePrice) - userBalance == creditCardStatus;
+
+// console.log(!paymentComplate);
+
+
+
+
+          /* <3 მეცრე დავალება */
+
+// let availableRooms = 5;
+// let roomPrice = 20;
+// let discount = 10;
+// let userBalance = 100;
+// let userHasdiscount = false;
+// let userRoomCount = 2;
+
+// const paymentComplate = (availableRooms - roomPrice) - userBalance && userRoomCount;
+
+// console.log(!!paymentComplate);
+
+
+
+/* მე მაპატიე ზუკიჩ ეს საშინელებები რო წაიკითხე :დდ <3 */
